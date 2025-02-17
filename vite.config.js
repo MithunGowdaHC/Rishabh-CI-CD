c92ff7c1588faaa96aeae17c1845dbab517240c4
MithunGowda <mithungowdahc12a@gmail.com>
2025-02-17 22:30:45 +0530

    deployed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Rishabh-CI-CD/", // Change to your GitHub repo name
   plugins: [react()],
-  
-})
+});
 // vite.config.js
